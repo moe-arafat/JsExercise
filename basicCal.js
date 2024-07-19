d806733bede6832ba8859d6a5f17245e8cbb9fae
moe-arafat <moe_arafat@outlook.com>
2024-07-18 20:06:15 -0400

    basic calculator app

---
+++ basicCal.js
@@ -0,0 +1,21 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+console.log("Addition: " + add(10, 7));
+console.log("Subtraction: " + subtract(20, 10));
+console.log("Multiply: " + multiply(10, 9));
+console.log("Division: " + divide(25, 5));
+console.log("Division by zero: " + divide(10, 0));
